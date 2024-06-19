de82abca7d3a183ad6c81d8db22df5664aa63d3a
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2024-06-19 17:49:14 -0300

    feat: Criacao classes Mentoria e Curso
    Signed-off-by: DamasX <84998412+TarcisioDamascena@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.desafio.dominio.Curso;
+import com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+
+        curso.setTitulo("Curso Java orientação a objeto");
+        curso.setDescricao("Descrição do curso");
+        curso.setCargaHoraria(2);
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição da mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
     }
 }
